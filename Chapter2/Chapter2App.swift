25017fdc86d0509eeee75b2a694b42b14d6fd947
Ethan Ennis <ethan_ennis_8@icloud.com>
2022-02-02 11:25:51 -0500

    Initial Commit

---
+++ Chapter2/Chapter2App.swift
@@ -0,0 +1,17 @@
+//
+//  Chapter2App.swift
+//  Chapter2
+//
+//  Created by Ethan Ennis on 2/2/22.
+//
+
+import SwiftUI
+
+@main
+struct Chapter2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
